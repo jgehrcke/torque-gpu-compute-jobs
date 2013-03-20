b6c092c66f959b6c8a5d4875922361b0a06dcb0f
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-03-20 11:17:06 +0100

    Consolidate submit-gpu-job program's subprocess handling, minor changes.

--- torque-submit-gpu-job.py
+++ torque-submit-gpu-job.py
@@ -1,20 +1,24 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright 2013 Jan-Philip Gehrcke.
 
 """
-PBS/Torque job submission wrapper script.
+PBS/Torque GPU job submission script. Wraps Torque's qsub command.
 
 Usage:
+
 submit-gpu-job "command [arg1 [arg2]]" [-o outerrfile]
 
-It basically does:
+In Bashism, it does:
 
-    echo "python compute-job-wrapper.py 'command' $PWD [-o ]" | \
+    echo "python torque-gpu-job-wrapper 'command' $PWD outerrfile " | \
         qsub -l nodes=1:gpus=1:ppn=1 -d $PWD
 
-The wrapper on the executing node ensures that stdout and stderror are
-collected and that the environment variable CUDA_VISIBLE_DEVICES is set
-according to the GPU assigned by PBS/Torque.
+The wrapper on the executing node ensures that the environment variable
+CUDA_VISIBLE_DEVICES is set according to the GPU assigned by PBS/Torque.
+Furthermore it collects stdout and stderr ob the job script to a file in the
+submission directory. If the file name is not explicitly given, a unique one is
+chosen.
 """
 
 
@@ -25,14 +29,16 @@ import os
 
 
 def main():
-    u = "\n\n%prog 'shell command' [-o output-file-path]"
+    u = "\n\n%prog 'shell command' [-o output-filename]"
     d = ("Submit job to Torque GPU queue. The shell command argument must be "
          "properly quoted. The working directory of the job is the current "
          "working directory.")
     parser = OptionParser(usage=u, description=d)
-    parser.add_option("-o", "--output-file", dest="output_file_path",
-        help="The stdout and stderr of the job is written to this file.",
-        metavar="PATH"
+    parser.add_option("-o", "--output", dest="output_filename",
+        help=("stdout and stderr of the job will be written to this file in "
+              "the current working directory. If not provided, a unique name "
+              "is chosen."),
+        metavar="FILENAME"
         )
 
     # Get, validate and process user input.
@@ -48,16 +54,21 @@ def main():
             sys.exit("'%s' already exists. Exit." % options.output_file_path)
         output_file_path = options.output_file_path
 
-    # The torque-compute-job-wrapper expects two or three arguments:
-    # "shell command lala lulu" "working_directory" [stdout_stderr_file]
+    # TODO: validate output filename, do not allow path separators, set
+    # output_filename.
 
-    shell_command = ("""echo "torque-compute-job-wrapper '%s' %s %s" | qsub -l nodes=1:gpus=1:ppn=1 -d %s""" %
-        (user_shell_command, cwd, output_file_path, cwd))
+    # The torque-compute-job-wrapper expects three arguments:
+    # "shell command lala lulu" "working_directory" "stdout_stderr_filename"
 
-    print shell_command
+    qsub_stdin = """torque-gpu-job-wrapper '%s' %s %s""" % (
+        user_shell_command, cwd, output_filename)
 
+    qsub_command = ['qsub', '-l', 'nodes=1:gpus=1:ppn=1', '-d', cwd]
 
-    returncode = subprocess.call(args=shell_command, shell=True)
+    sp = subprocess.Popen(args=qsub_command)
+    out, err = sp.communicate(qsub_stdin)
+
+    returncode = sp.returncode
 
 if __name__ == "__main__":
     main()
